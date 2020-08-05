a1906e582f5bc6f1c507baa431fc74bd605b77cd
denbell <denbell@ukr.net>
2020-08-05 20:33:35 +0300

    Test hub methods subscriptions

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { WebrtcSignalService } from './core/services/webrtc-signal.service'
+import { WebrtcSignalService, SignalData } from './core/services/webrtc-signal.service'
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,27 @@ export class AppComponent {
   title = 'frontend';
 
   constructor(private signalHubService: WebrtcSignalService) {
-    
+    this.signalHubService.newClientConnected$.subscribe((newConnectionId) => {
+      console.log(`New user connected: ${newConnectionId}`);
+      // send signal to new user here ?
+      this.signalHubService.signalHub.send('SignalOffer', newConnectionId, "fakeOffer");
+    });
+
+    this.signalHubService.clientDisconnected$.subscribe((disconnectedConnectionId) => {
+      console.log(`User disconnected: ${disconnectedConnectionId}`);
+      // remove tracks from disconnected user here ?
+    });
+
+    this.signalHubService.signalOffer$.subscribe((signalData: SignalData) => {
+      console.log(`Signal OFFER from: ${signalData.fromConnectionId}`);
+      // handle offer, create and send answer here ?
+      this.signalHubService.signalHub.send('SignalAnswer', signalData.fromConnectionId, "fakeAnswer");
+    });
+
+    this.signalHubService.signalAnswer$.subscribe((signalData: SignalData) => {
+      console.log(`Signal ANSWER from: ${signalData.fromConnectionId}`);
+      // handle answer here ?
+    });
   }
 
 }
