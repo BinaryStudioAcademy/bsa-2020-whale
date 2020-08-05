23e5fb5ad797efce93bf0a8056a478b9620e3783
Anton Komisarov <apofis96@gmail.com>
2020-08-05 23:42:25 +0300

    Frontend Auth done

--- frontend/src/app/core/auth/auth.service.ts
+++ frontend/src/app/core/auth/auth.service.ts
@@ -7,18 +7,15 @@ import { AngularFireAuth } from '@angular/fire/auth';
   })
 export class AuthService {
   private authUser: firebase.User = null;
-  private cachedToken: string = null;
 
   constructor(private fireAuth: AngularFireAuth) {
       fireAuth.authState.subscribe(
         (user) => {
           if (user) {
             this.authUser = user;
-            user.getIdToken().then(token => this.cachedToken = token);
           }
           else {
             this.authUser = null;
-            this.cachedToken = null;
           }
         }
       );
@@ -39,12 +36,11 @@ export class AuthService {
   public logout(): Promise<void> {
     return this.fireAuth.signOut();
   }
-  public getAccessToken(): string {
-    return this.cachedToken;
-  }
 
-  public async refreshToken(refresh: boolean = true): Promise<string> {
-    const token = await this.authUser?.getIdToken(refresh);
-    return this.cachedToken = token;
+  public getToken(): Promise<string> {
+    return this.authUser?.getIdToken();
+  }
+  public refreshToken(): Promise<string> {
+    return this.authUser?.getIdToken(true);
   }
 }
--- frontend/src/app/core/auth/token-interceptor.service.ts
+++ frontend/src/app/core/auth/token-interceptor.service.ts
@@ -1,136 +1,58 @@
-import { Injectable, Injector } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Observable, Subject, throwError } from 'rxjs';
-import { tap, catchError } from 'rxjs/operators';
-
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { Observable, throwError, from } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor {
+  constructor(private authService: AuthService) {}
 
-    refreshTokenInProgress = false;
+  inflightAuthRequest = null;
 
-    tokenRefreshedSource = new Subject();
-    tokenRefreshed$ = this.tokenRefreshedSource.asObservable();
+  intercept( req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    constructor(private injector: Injector, private router: Router, private authService: AuthService) {}
-
-    addAuthHeader(request) {
-        const authHeader = this.authService.getAccessToken();
-        if (authHeader) {
-            return request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${authHeader}.`
-                }
-            });
-        }
-        return request;
+    if (!this.inflightAuthRequest) {
+      this.inflightAuthRequest = from(this.authService.getToken());
     }
 
-    async handleResponseError(error, request?, next?) {
-      // Invalid token error
-      console.log('test');
-      if (error.status === 401) {
-        const newToken = await this.authService.refreshToken();
-        if (newToken) {
-          request = this.addAuthHeader(request);
-          return next.handle(request).pipe(catchError(e => {
-            if (e.status !== 401) {
-                return this.handleResponseError(e);
-            } else {
-              console.log('logout');
-                //this.authService.logout();
-            }}));
-        }
-         /* return this.refreshToken().pipe(
-              switchMap(() => {
-                  request = this.addAuthHeader(request);
-                  return next.handle(request);
-              }),
-              catchError(e => {
-                  if (e.status !== 401) {
-                      return this.handleResponseError(e);
-                  } else {
-                      this.logout();
-                  }
-              }));
-      }*/
-
-      // Access denied error
-      
-
-      //return throwError(error);
-  }
-    }
-
-  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<any> {
-      request = this.addAuthHeader(request);
-
-      // Handle response
-      return next.handle(request).pipe(catchError(error => {
-          return this.handleResponseError(error, request, next);
-      }));
-  }
-
-  
-
- /* intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = this.authService.getAccessToken();
-    if (this.authService.isLoggedIn() && token) {
-      return next.handle(req.clone({
-        headers: req.headers.set('Authentication', `Bearer ${token}`)
-      })).pipe(
-        tap(
-          inext => {
-            if (inext instanceof HttpResponse) {
-              console.log('Server response', inext);
-            }
-          },
-          async error => {
-            console.log('ok?');
-            if (error instanceof HttpErrorResponse) {
-              if (error.status === 401){
-                console.log('okk?');
-                const refreshedToken = await this.authService.refreshToken();
-                if (refreshedToken) {
-                  return  next.handle(req.clone({
-                    headers: req.headers.set('Authentication', `Bearer ${refreshedToken}`)
-                  }));
-              }
-            }
-              else
-                console.log('Error', error);
+    return this.inflightAuthRequest.pipe(
+      switchMap((token: string) => {
+        this.inflightAuthRequest = null;
+        const authReq = req.clone({
+          setHeaders: {
+              Authorization: `Bearer ${token}`
+          }
+        });
+        return next.handle(authReq);
+      }),
+      catchError(error => {
+        if (error.status === 401) {
+          if (!this.inflightAuthRequest) {
+            this.inflightAuthRequest = from(this.authService.refreshToken());
+            if (!this.inflightAuthRequest) {
+              return throwError(error);
             }
           }
-        )
-      );
-    }
-    else
-      return next.handle(req);
+          return this.inflightAuthRequest.pipe(
+            switchMap((newToken: string) => {
+              this.inflightAuthRequest = null;
+              const authReqRepeat = req.clone({
+                setHeaders: {
+                    Authorization: `Bearer ${newToken}`
+                }
+              });
+              return next.handle(authReqRepeat);
+            })
+          );
+        }
+        else {
+          return throwError(error);
+        }
+      })
+    );
   }
-
-  private handleUnauthorizedError(subscriber: Subscriber < any >, request: HttpRequest<any>) {
-
-    //запоминаем "401ый" запрос
-    this.requests.push({ subscriber, failedRequest: request });
-    if(!this.refreshInProgress) {
-      //делаем запрос на восстанавливение токена, и установим флаг, дабы следующие "401ые"
-      //просто запоминались но не инициировали refresh
-      this.refreshInProgress = true;
-      this.auth.renewAuth()
-        .finally(() => {
-          this.refreshInProgress = false;
-        })
-        .subscribe((authHeader) =>
-          //если токен рефрешнут успешно, повторим запросы которые накопились пока мы ждали ответ от рефреша
-          this.repeatFailedRequests(authHeader),
-          () => {
-            //если по каким - то причинам запрос на рефреш не отработал, то делаем логаут
-            this.auth.logout();
-          });
-    }
-  }*/
 }
