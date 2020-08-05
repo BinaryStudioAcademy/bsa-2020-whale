a54d58410e3c7afc67f9153b1c9a9d4f4a9dcad8
denbell <denbell@ukr.net>
2020-08-05 19:27:17 +0300

    Add WebrtcSignalService to app.component.ts for tests

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebrtcSignalService } from './core/services/webrtc-signal.service'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontend';
+
+  constructor(private signalHubService: WebrtcSignalService) {
+    
+  }
+
 }
