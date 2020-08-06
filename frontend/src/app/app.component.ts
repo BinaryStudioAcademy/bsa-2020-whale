ca9144b58d5627d172f76c11ef3ba83f68025094
denbell <denbell@ukr.net>
2020-08-06 09:45:12 +0300

    Clear app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -8,29 +8,4 @@ import { WebrtcSignalService, SignalData } from './core/services/webrtc-signal.s
 })
 export class AppComponent {
   title = 'frontend';
-
-  constructor(private signalHubService: WebrtcSignalService) {
-    this.signalHubService.newClientConnected$.subscribe((newConnectionId) => {
-      console.log(`New user connected: ${newConnectionId}`);
-      // send signal to new user here ?
-      this.signalHubService.signalHub.send('SignalOffer', newConnectionId, "fakeOffer");
-    });
-
-    this.signalHubService.clientDisconnected$.subscribe((disconnectedConnectionId) => {
-      console.log(`User disconnected: ${disconnectedConnectionId}`);
-      // remove tracks from disconnected user here ?
-    });
-
-    this.signalHubService.signalOffer$.subscribe((signalData: SignalData) => {
-      console.log(`Signal OFFER from: ${signalData.fromConnectionId}`);
-      // handle offer, create and send answer here ?
-      this.signalHubService.signalHub.send('SignalAnswer', signalData.fromConnectionId, "fakeAnswer");
-    });
-
-    this.signalHubService.signalAnswer$.subscribe((signalData: SignalData) => {
-      console.log(`Signal ANSWER from: ${signalData.fromConnectionId}`);
-      // handle answer here ?
-    });
-  }
-
 }
