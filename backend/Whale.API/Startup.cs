db705ce318c1ffc409c14242590d8da8c1de1257
Ostap Kernytskyi <okernytskiy@gmail.com>
2020-08-06 14:27:27 +0300

    chatHub updated

--- backend/Whale.API/Startup.cs
+++ backend/Whale.API/Startup.cs
@@ -58,7 +58,6 @@ namespace Whale.API
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
-                endpoints.MapHub<ChatHub>("/chatHub");
             });
         }
     }
