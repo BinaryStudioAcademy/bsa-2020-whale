1e0c590e39b2425b27064b6ca29e25ae37c74c62
Olya Melnyk <olga14.olg@gmail.com>
2020-08-10 14:45:37 +0300

    implement exception middleware

---
+++ backend/Whale.API/Models/ErrorDetails.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Whale.API.Models
+{
+    public class ErrorDetails
+    {
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
