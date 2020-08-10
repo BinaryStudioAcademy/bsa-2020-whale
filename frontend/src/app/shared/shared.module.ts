99619d495686462a0a9600a1b33b9de320e1afcd
325Vitalik <mr.yarmus@gmail.com>
2020-08-10 15:20:27 +0300

    Add spiner

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './components/chat/chat.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { SpinerComponent } from './components/spiner/spiner.component';
 
 
 
 @NgModule({
-  declarations: [ChatComponent, PageHeaderComponent],
+  declarations: [
+    ChatComponent, 
+    PageHeaderComponent, 
+    SpinerComponent
+  ],
   imports: [
     CommonModule
   ],
   exports: [
     CommonModule,
     ChatComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    SpinerComponent
   ]
 })
 export class SharedModule { }
