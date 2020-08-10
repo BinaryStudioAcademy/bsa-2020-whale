04be30513fb471a08b6bff7ec4885f89814926aa
denbell <denbell@ukr.net>
2020-08-10 18:59:40 +0300

    Update shared.module.ts

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatComponent } from './components/chat/chat.component';
 import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { PollCreateComponent } from './components/poll/poll-create/poll-create.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PollComponent } from './components/poll/poll/poll.component';
 
 
 
 @NgModule({
-  declarations: [ChatComponent, PageHeaderComponent],
+  declarations: [ChatComponent, PageHeaderComponent, PollCreateComponent, PollComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule
   ],
   exports: [
     CommonModule,
     ChatComponent,
-    PageHeaderComponent
+    PageHeaderComponent,
+    PollCreateComponent,
+    PollComponent
   ]
 })
 export class SharedModule { }
