fd42748247b6ccc4f6cda4fe77ce5cb6d33df9d8
Iryna Kopyl <iruna.kopyl@gmail.com>
2020-08-13 02:20:13 +0300

    chat between contacts is done

--- backend/Whale.DAL/Migrations/20200812211446_DirectMsgModelFixes.Designer.cs
+++ backend/Whale.DAL/Migrations/20200812211446_DirectMsgModelFixes.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Whale.DAL;
 
 namespace Whale.DAL.Migrations
 {
     [DbContext(typeof(WhaleDbContext))]
-    partial class WhaleDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200812211446_DirectMsgModelFixes")]
+    partial class DirectMsgModelFixes
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -48,7 +50,7 @@ namespace Whale.DAL.Migrations
                     b.Property<Guid?>("FirstMemberSettingsId")
                         .HasColumnType("uniqueidentifier");
 
-                    b.Property<Guid?>("PinnedMessageId")
+                    b.Property<Guid>("PinnedMessageId")
                         .HasColumnType("uniqueidentifier");
 
                     b.Property<Guid>("SecondMemberId")
@@ -349,7 +351,9 @@ namespace Whale.DAL.Migrations
 
                     b.HasOne("Whale.DAL.Models.DirectMessage", "PinnedMessage")
                         .WithMany()
-                        .HasForeignKey("PinnedMessageId");
+                        .HasForeignKey("PinnedMessageId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
 
                     b.HasOne("Whale.DAL.Models.User", "SecondMember")
                         .WithMany()
--- backend/Whale.DAL/Models/Contact.cs
+++ backend/Whale.DAL/Models/Contact.cs
@@ -9,7 +9,7 @@ namespace Whale.DAL.Models
         public User FirstMember { get; set; }
         public Guid SecondMemberId { get; set; }
         public User SecondMember { get; set; }
-        public Guid PinnedMessageId { get; set; }
+        public Guid? PinnedMessageId { get; set; }
         public DirectMessage PinnedMessage { get; set; }
         public ContactSetting FirstMemberSettings { get; set; }
         public ContactSetting SecondMemberSettings { get; set; }
