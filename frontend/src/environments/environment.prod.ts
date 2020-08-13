e343d0d397292c449b6740df907ea88c38298f7b
Nikita Krasnov <nikita.krasnov@binary-studio.com>
2020-08-13 15:05:54 +0300

    Added production urls

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://bsa2020-whale.westeurope.cloudapp.azure.com:4201',
+  meetingApiUrl: 'http://bsa2020-whale.westeurope.cloudapp.azure.com:4202',
 };
