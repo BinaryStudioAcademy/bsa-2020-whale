aebfa8f5b8c2cf35b9dcba0f953dfd90eec43848
Smyrechynskyi <41690469+Smyrechynskyi@users.noreply.github.com>
2020-08-13 23:37:37 +0300

    Revert commit 8f15af7 13 Aug 18:30

--- backend/Whale.BLL/Services/MeetingService.cs
+++ backend/Whale.BLL/Services/MeetingService.cs
@@ -118,6 +118,7 @@ namespace Whale.BLL.Services
                 throw new NotFoundException("Participant");
 
             var isHost = participant.Role == Shared.DTO.Participant.ParticipantRole.Host;
+            await _participantService.DeleteParticipantAsync(participant.Id);
             if (isHost)
             {
                 await _redisService.ConnectAsync();
