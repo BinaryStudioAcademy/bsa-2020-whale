cf59fb3c7671317c5aa64818e8d54bff22654bb6
325Vitalik <mr.yarmus@gmail.com>
2020-08-14 09:38:32 +0300

    Fix bug with participants

--- frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
+++ frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
@@ -109,7 +109,6 @@ export class MeetingComponent
       .subscribe(
         (connectData) => {
           console.log(connectData);
-          this.meeting.participants.push(connectData.participant);
           console.log(this.meeting);
           if (connectData.peerId == this.peer.id) {
             return;
@@ -130,8 +129,6 @@ export class MeetingComponent
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(
         (participants) => {
-          participants.forEach((p) => this.meeting.participants.push(p));
-
           this.currentParticipant = participants.find(
             (p) => p.user.email === this.authService.currentUser.email
           );
