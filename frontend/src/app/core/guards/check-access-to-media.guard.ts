48356cdd679bc236cc7eb53f907e65db615701e1
Vitalii Yarmus <55307415+325Vitalik@users.noreply.github.com>
2020-08-17 16:56:05 +0300

    fix infinity reloading

--- frontend/src/app/core/guards/check-access-to-media.guard.ts
+++ frontend/src/app/core/guards/check-access-to-media.guard.ts
@@ -9,27 +9,31 @@ import {
 import { Observable, from } from 'rxjs';
 import { tap, map, catchError } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
+import { MediaSettingsService } from '../services/media-settings.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckAccessToMediaGuard implements CanActivate {
-  constructor(private router: Router, private toastr: ToastrService) {}
+  constructor(
+    private router: Router,
+    private toastr: ToastrService,
+    private mediaSettingsService: MediaSettingsService
+  ) {}
 
   async canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Promise<boolean | UrlTree> {
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true,
-      });
+      const stream = await navigator.mediaDevices.getUserMedia(
+        await this.mediaSettingsService.getMediaConstraints()
+      );
       return stream.active;
     } catch {
       alert('Cannot access the camera and microphone');
-      window.location.reload();
-      return this.router.createUrlTree(['/home']);
+      if (window.location.pathname == '/home') window.location.reload();
+      return this.router.navigate(['/home']);
     }
   }
 }
