dca278cab4c4a67fd0b737ed8b186afd5da57288
Ostap Kernytskyi <okernytskiy@gmail.com>
2020-08-18 12:28:22 +0300

    Duplicate participants fixed

--- frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
+++ frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
@@ -177,7 +177,7 @@ export class MeetingComponent
       .pipe(takeUntil(this.unsubscribe$))
       .subscribe(
         (participants) => {
-          this.meeting.participants.push(...participants);
+          //this.meeting.participants.push(...participants);
           this.currentParticipant = participants.find(
             (p) => p.user.email === this.authService.currentUser.email
           );
