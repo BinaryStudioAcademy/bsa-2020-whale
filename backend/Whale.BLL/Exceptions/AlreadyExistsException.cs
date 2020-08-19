6d6feecfa2f1701eb9dcec4ef39352b148beb4e2
Alexander Belokon <sascha.belokon@gmail.com>
2020-08-19 10:24:04 +0300

    hotfix exists exception

--- backend/Whale.BLL/Exceptions/AlreadyExistsException.cs
+++ backend/Whale.BLL/Exceptions/AlreadyExistsException.cs
@@ -6,10 +6,14 @@ namespace Whale.BLL.Exceptions
     {
         public AlreadyExistsException(string name, string id)
             : base($"{name} with {id} is already exist.")
-        { }
+        {
+            _httpError = 400;
+        }
         public AlreadyExistsException(string name)
             : base($"{name} is already exist.")
-        { }
+        {
+            _httpError = 400;
+        }
 
         public AlreadyExistsException() : base()
         {
