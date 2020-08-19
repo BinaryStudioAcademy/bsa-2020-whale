6551c96a0f292c6366f4ac308b9b507f08f85f0a
Übermensch <genebloch00@gmail.com>
2020-08-19 13:07:22 +0300

    Redirection page redesign

--- frontend/src/app/scenes/pages.module.ts
+++ frontend/src/app/scenes/pages.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SettingPageModule } from './setting-page/setting-page.module';
 import { RedirectionComponent } from './redirection-page/redirection/redirection.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [RedirectionComponent],
-  imports: [CommonModule, SettingPageModule],
+  imports: [CommonModule, SettingPageModule, SharedModule],
 })
 export class PagesModule {}
