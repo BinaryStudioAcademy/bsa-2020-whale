5108685b924685721126e1d1c71d82c7c9045628
Übermensch <genebloch00@gmail.com>
2020-08-22 11:59:57 +0300

    Implemented slack bot (start meeting command)

--- backend/Whale.API/Controllers/SlackController.cs
+++ backend/Whale.API/Controllers/SlackController.cs
@@ -1,32 +1,48 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Whale.API.Models;
 using Whale.API.Services;
+using System;
+using Whale.Shared.Models.Meeting;
+using Whale.API.Models.Slack;
 
-namespace Whale.API.Controllers
+namespace Whale.API.Controllers.Slack
 {
     public class SlackController : ControllerBase
     {
         private readonly SlackService _slackService;
+        private readonly HttpService _httpService;
+        private readonly string _baseURL;
 
-        public SlackController(SlackService slackService)
+        public SlackController(SlackService slackService, HttpService httpService)
         {
             _slackService = slackService;
+            _httpService = httpService;
+
+            _baseURL = "http://bsa2020-whale.westeurope.cloudapp.azure.com";
         }
 
-        [Route("/api/slack/command")]
+        [Route("/api/slack/startMeeting")]
         [HttpPost]
         [Produces("application/json")]
-        public async Task<string> StartMeeting(SlackCommand userData)
+        public async Task StartMeeting(SlackCommand userData)
         {
-            var userEmail = userData.Text;
+            var userEmail = userData.text;
 
-            var isValidEmail = _slackService.IsValidEmail(userEmail);
+            if (userEmail == null) await _slackService.SendSlackReplyAsync("You should to specify email. (ex. /whale user@gmail.com)", userData.response_url);
 
-            if (isValidEmail)
-                return "Correct email";
+            var meetingDTO = new MeetingCreateDTO() { CreatorEmail = userEmail, IsScheduled = false };
 
-            return "Incorrect email";
+            try
+            {
+                var meetingLinkDTO = await _httpService.PostAsync<MeetingCreateDTO, MeetingLinkDTO>("api/meeting", meetingDTO);
+                var link = $"{_baseURL}/meeting-page/%3Fid%3D{meetingLinkDTO.Id}&pwd%3D{meetingLinkDTO.Password}";
+
+                await _slackService.SendSlackReplyAsync(link, userData.response_url);
+            }
+            catch(Exception)
+            {
+                await _slackService.SendSlackReplyAsync("Some troubles happened", userData.response_url);
+            }
         }
     }
 }
---
+++ backend/Whale.API/Models/Slack/SlackReply.cs
@@ -0,0 +1,7 @@
+﻿namespace Whale.API.Models.Slack
+{
+    public class SlackReply
+    {
+        public string text { get; set; }
+    }
+}
