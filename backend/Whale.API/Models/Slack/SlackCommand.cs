3d620aee13739f46c5be9c2645392490bd5b9220
Übermensch <genebloch00@gmail.com>
2020-08-25 20:04:33 +0300

    Send messages to private channels

--- backend/Whale.API/Models/Slack/SlackCommand.cs
+++ backend/Whale.API/Models/Slack/SlackCommand.cs
@@ -3,6 +3,6 @@
     public class SlackCommand
     {
         public string text { get; set; }
-        public string channel_name { get; set; }
+        public string channel_id { get; set; }
     }
 }
