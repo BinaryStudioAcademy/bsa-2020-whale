e8b7c7be42dc5f896528cedebeb01d662f0c8ec2
alex-bilokon <alex.belokon.onyx@gmail.com>
2020-09-01 00:59:09 +0300

    added models

---
+++ backend/Whale.API/Models/Slack/ExternalCommand.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Whale.API.Models.Slack
+{
+    public class ExternalCommand
+    {
+        public string text { get; set; }
+        public string email { get; set; }
+    }
+}
