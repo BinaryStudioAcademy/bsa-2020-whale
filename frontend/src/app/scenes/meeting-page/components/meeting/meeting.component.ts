f78ab11a62f0e1b6c664843301304e554b57d815
Anton Komisarov <32576664+apofis96@users.noreply.github.com>
2020-09-01 17:56:58 +0300

    Revert "Webspeech test"

--- frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
+++ frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
@@ -67,7 +67,6 @@ import { Question } from '@shared/models/question/question';
 import { MeetingSettingsService } from '../../../../core/services/meeting-settings.service';
 import { MeetingInviteModalData } from '@shared/models/email/meeting-invite-modal-data';
 
-declare var webkitSpeechRecognition: any;
 
 @Component({
   selector: 'app-meeting',
@@ -77,7 +76,6 @@ declare var webkitSpeechRecognition: any;
 export class MeetingComponent
   implements OnInit, AfterViewInit, OnDestroy, AfterViewChecked {
   //#region fields
-  public recognition: any;
   public canvasIsDisplayed = false;
   public canvasOptions: CanvasWhiteboardOptions = {
     clearButtonEnabled: true,
@@ -938,21 +936,7 @@ export class MeetingComponent
   }
 
   public onStatisticsIconClick(): void {
-    this.recognition = new webkitSpeechRecognition();
-    this.recognition.continuous = true;
-    this.recognition.lang = 'ru';
-    this.recognition.interimResults = false;
-    this.recognition.maxAlternatives = 1;
-
-    if (!this.recognition.onresult) {
-      this.recognition.onresult = (event) => { this.handleResultEvent(event); };
-    }
-
-    if (!this.recognition.onend) {
-      this.recognition.onend = (event) => { this.handleEndEvent(event); };
-    }
-    this.recognition.start();
-    /*this.isShowReactions = false;
+    this.isShowReactions = false;
     this.pollService.isShowPoll = false;
     this.isShowMeetingSettings = false;
     this.pollService.isPollCreating = false;
@@ -970,18 +954,8 @@ export class MeetingComponent
         userJoinTime: this.contectedAt,
       };
     }
-    this.isShowStatistics = !this.isShowStatistics;*/
+    this.isShowStatistics = !this.isShowStatistics;
   }
-  private handleResultEvent(event: any): void {
-    console.info('Event.');
-    console.info(event.results);
-  }
-
-  private handleEndEvent(event: any): void {
-    console.info('End.');
-    console.info(event);
-  }
-
   public onReactionsIconClick(): void {
     this.pollService.isShowPoll = false;
     this.pollService.isPollCreating = false;
