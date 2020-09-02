6fb110c51893f5cbf05029d23fc26064e5ec3e07
denbell <denbell@ukr.net>
2020-09-02 11:03:22 +0300

    Add anonymous questions

--- frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
+++ frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
@@ -1494,9 +1494,6 @@ export class MeetingComponent
       });
       this.isNewMsg = false;
     }
-
-    // this.isNewMsg = !(this.isShowChat && this.isChat);
-    // console.log(this.isNewMsg);
   }
 
   public sendMessage(): void {
