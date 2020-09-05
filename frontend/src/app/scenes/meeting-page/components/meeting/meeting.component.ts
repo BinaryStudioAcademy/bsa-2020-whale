29a2ed722b8bfdf9f7094089f0f598568d43b0a1
denbell <denbell@ukr.net>
2020-09-05 14:20:37 +0300

    Return speech recignition and await methods

--- frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
+++ frontend/src/app/scenes/meeting-page/components/meeting/meeting.component.ts
@@ -925,12 +925,12 @@ export class MeetingComponent
   //#endregion hooks
 
   //#region options
-  public toggleMicrophone(isMissSignaling: boolean = false): void {
+  public async toggleMicrophone(isMissSignaling: boolean = false): Promise<void> {
     if (
       !this.meeting.isAudioAllowed &&
       this.currentParticipant?.role !== ParticipantRole.Host
     ) {
-      this.switchTrack(false, false);
+      await this.switchTrack(false, false);
       this.isMicrophoneMuted = true;
       this.isRecognitionStop = true;
       this.recognition?.stop();
@@ -938,8 +938,8 @@ export class MeetingComponent
     }
 
     this.isMicrophoneMuted
-      ? this.switchTrack(true, false)
-      : this.switchTrack(false, false);
+      ? await this.switchTrack(true, false)
+      : await this.switchTrack(false, false);
 
     this.isMicrophoneMuted = !this.isMicrophoneMuted;
     this.isRecognitionStop = this.isMicrophoneMuted;
@@ -949,19 +949,17 @@ export class MeetingComponent
     }
   }
 
-  public toggleCamera(isMissSignaling: boolean = false): void {
+  public async toggleCamera(isMissSignaling: boolean = false): Promise<void> {
     if (
       !this.meeting.isVideoAllowed &&
       this.currentParticipant?.role !== ParticipantRole.Host
     ) {
-      this.switchTrack(false, true);
+      await this.switchTrack(false, true);
       this.isCameraMuted = true;
       return;
     }
 
-    this.isCameraMuted
-      ? this.switchTrack(true, true)
-      : this.switchTrack(false, true);
+    await this.switchTrack(this.isCameraMuted, true);
 
     this.isCameraMuted = !this.isCameraMuted;
     if (!isMissSignaling) {
@@ -969,7 +967,7 @@ export class MeetingComponent
     }
   }
 
-  private switchTrack(enable: boolean, isVideo: boolean): void {
+  private async switchTrack(enable: boolean, isVideo: boolean): Promise<void> {
     const tracks = isVideo
       ? this.currentUserStream.getVideoTracks()
       : this.currentUserStream.getAudioTracks();
@@ -1303,7 +1301,7 @@ export class MeetingComponent
 
             this.questionService.getQuestionsByMeeting(this.meeting.id);
 
-            // this.configureRecognition();
+            this.configureRecognition();
           });
         },
         (error) => {
@@ -2045,10 +2043,10 @@ export class MeetingComponent
       .subscribe(
         (event) => {
           if (!this.isRecognitionStop) {
-            // this.recognition.start();
+            this.recognition.start();
           }
         });
-      // this.recognition.start();
+      this.recognition.start();
     }
     catch {
       this.toastr.info('Speech recognition is not supported by the browser');
